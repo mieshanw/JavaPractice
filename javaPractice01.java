2286dba825a50979c53ea5a97beddda42bb8624b
mieshanw <mieshanw@gmail.com>
2018-03-05 17:02:07 -0500

    TreeStory

---
+++ javaPractice01.java
@@ -0,0 +1,51 @@
+import java.io.Console;
+ 
+public class TreeStory {
+    
+    public static void main(String[] args) {
+        Console console = System.console();
+        /*  Some terms:
+            noun - Person, place or thing
+            verb - An action
+            adjective - A description used to modify or describe a noun
+            Enter your amazing code here!
+        */
+      /*String firstName = console.readLine("Enter your first name:  "); 
+      String adjective = console.readLine("Enter a adjective:  "); 
+      console.printf("%s is very %s", firstName, adjective);
+    */
+      String ageAsString = console.readLine("How old are you?  "); 
+      int age = Integer.parseInt(ageAsString); 
+      if(age < 13) {
+        //insert exit code 
+        console.printf("Sorry you must be at least 13 to use this program\n"); 
+        System.exit(0);
+        
+      }
+      String name = console.readLine("Enter a name:  ");
+      String adjective = console.readLine("Enter an adjective:  ");
+      String noun;
+      boolean isInvalidWord;
+      do {
+      noun = console.readLine("Enter a noun:  ");
+      isInvalidWord = (noun.equalsIgnoreCase("dork") || 
+          noun.equalsIgnoreCase("jerk") ||
+          noun.equalsIgnoreCase("nerd")); 
+        if (isInvalidWord) {
+        console.printf("That language is not allowed. Try Again \n\n");
+      
+      }
+      }   while(noun.equalsIgnoreCase("dork") || 
+            noun.equalsIgnoreCase("jerk") ||
+            noun.equalsIgnoreCase("nerd")); {
+              
+            }
+          String adverb = console.readLine("Enter an adverb:  ");
+          String verb = console.readLine("Enter a verb ending with -ing:  ");
+   
+      console.printf("Your Treestory:\n-------------\n");
+      console.printf("%s is a %s %s.  ", name, adjective, noun); 
+      console.printf("They are always %s %s.\n", adverb, verb); 
+    }
+    
+}
